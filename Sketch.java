49225d9743205a203a105798f1ecc87e35e44458
Oscar Lin <78488962+oscarlin05@users.noreply.github.com>
2022-02-21 22:33:43 +0000

    final submission

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(34, 206, 245);
   }
 
   /**
@@ -24,12 +24,52 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	//ground color (foreground)
+    stroke(74, 50, 1);
+    fill(74, 50, 1);
+    rect(0, 650, 800, 200);
+    
+  //lawn (foreground)
+    stroke(0);
+    fill(22, 181, 72);
+    rect(150, 650, 550, 500);
+    //rect(600, 650, 250, 50);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  //pathway (foreground)
+    stroke(0);
+    fill(130, 148, 148);
+    rect(390, 650, 80, 200);    
+    
+  //house shape
+    stroke(0);
+    fill(247, 186, 62);
+    rect(250, 300, 350, 350);
+
+  //roof
+    stroke(0);
+    fill(153, 99, 0);
+    triangle(150, 300, 425, 100, 700, 300);
+    
+  //door
+    stroke(0);
+    rect(390, 550, 80, 100);
+    fill(74, 50, 1);
+    ellipse(460, 610, 15, 15);
+
+  //window
+    stroke(0);
+    fill(185, 250, 249);
+    rect(390, 350, 80, 80);
+    
+    line(390, 390, 470, 390);
+    line(430, 350, 430, 430);
+
+  //sun
+    stroke(0);
+    fill(250, 225, 0);
+    ellipse(750, 50, 100, 100);
+
+
   }
   
   // define other methods down here.
