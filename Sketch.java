b7323b0519c017fa35effcd47f901207e790db80
Oscar Lin <78488962+oscarlin05@users.noreply.github.com>
2022-02-25 04:05:25 +0000

    updated programming style

--- Sketch.java
+++ Sketch.java
@@ -24,39 +24,38 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	//ground color (foreground)
+	// ground color (foreground)
     stroke(74, 50, 1);
     fill(74, 50, 1);
     rect(0, 650, 800, 200);
     
-  //lawn (foreground)
+  // lawn (foreground)
     stroke(0);
     fill(22, 181, 72);
     rect(150, 650, 550, 500);
-    //rect(600, 650, 250, 50);
 
-  //pathway (foreground)
+  // pathway (foreground)
     stroke(0);
     fill(130, 148, 148);
     rect(390, 650, 80, 200);    
     
-  //house shape
+  // house shape
     stroke(0);
     fill(247, 186, 62);
     rect(250, 300, 350, 350);
 
-  //roof
+  // roof
     stroke(0);
     fill(153, 99, 0);
     triangle(150, 300, 425, 100, 700, 300);
     
-  //door
+  // door
     stroke(0);
     rect(390, 550, 80, 100);
     fill(74, 50, 1);
     ellipse(460, 610, 15, 15);
 
-  //window
+  // window
     stroke(0);
     fill(185, 250, 249);
     rect(390, 350, 80, 80);
@@ -64,7 +63,7 @@ public class Sketch extends PApplet {
     line(390, 390, 470, 390);
     line(430, 350, 430, 430);
 
-  //sun
+  // sun
     stroke(0);
     fill(250, 225, 0);
     ellipse(750, 50, 100, 100);
